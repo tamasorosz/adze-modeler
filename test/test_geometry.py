2c80be9d8bfa6ca3333dada7efd8d544a20a4ad3
tamas <orosz.tamas@yahoo.com>
2021-01-23 14:33:48 +0100

    first tests

--- test/test_geometry.py
+++ test/test_geometry.py
@@ -1,1 +1,7 @@
-import artap_segment.geometry
+from unittest import TestCase
+from artap_segment.objects import Node
+
+class TestNodeOperations(TestCase):
+
+    def test_node_init(self):
+        self.assertEqual( (1., 1.),Node(1., 1.).as_tuple())
