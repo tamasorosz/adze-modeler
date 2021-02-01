b14de444d3a68e066311f721cd78a74d486f4da3
Tamas Orosz <oroszt@montana.hu>
2021-02-01 17:01:40 +0100

    using importlib_resources to define the path for resources

--- tests/test_svg_handlers.py
+++ tests/test_svg_handlers.py
@@ -1,13 +1,18 @@
 from unittest import TestCase
 from artapsegment.svg_handlers import import_svg
-import os
+from importlib_resources import files
+
+
+# Reads contents with UTF-8 encoding and returns str.
+# eml = files('email.tests.data').joinpath('message.eml').read_text()
 
 
 class TestSvgImport(TestCase):
 
     def test_owl_import_to_geometry(self):
-        #path = os.path.join(ROOT_DIR, "examples/owl", "owl-svgrepo-com.svg")
-        geo = import_svg("artap-segment/tests/test.svg")
+        eml = files('examples.triangle').joinpath('triangle.svg')
+        print(eml)
+        geo = import_svg(eml.as_posix())
 
         # checks the first coordinate of the first node
         self.assertEqual(82.020832, geo.nodes[0].x)
