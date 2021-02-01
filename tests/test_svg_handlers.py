af57cdb5abf8c1939a780499d86bf5f7285faf49
Tamas Orosz <oroszt@montana.hu>
2021-02-01 21:16:50 +0100

    requirements.txt added to the tox.ini

--- tests/test_svg_handlers.py
+++ tests/test_svg_handlers.py
@@ -10,14 +10,14 @@ from importlib_resources import files
 class TestSvgImport(TestCase):
 
     def test_owl_import_to_geometry(self):
-        eml = files('examples.triangle').joinpath('triangle.svg')
+        eml = files('examples.owl').joinpath('owl-svgrepo-com.svg')
         print(eml)
         geo = import_svg(eml.as_posix())
 
         # checks the first coordinate of the first node
-        self.assertEqual(82.020832, geo.nodes[0].x)
+        self.assertEqual(445.642, geo.nodes[0].x)
 
         # the number of lines and cubicbeziers should be larger than 0
         self.assertTrue(len(geo.lines) > 0)
-        self.assertTrue(len(geo.cubic_beziers) == 0)
+        self.assertTrue(len(geo.cubic_beziers) > 0)
         self.assertTrue(len(geo.circle_arcs) == 0)
