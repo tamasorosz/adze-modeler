67f945f53f3f1039f4f588c9495d278091b03074
Tamas Orosz <oroszt@montana.hu>
2021-02-01 15:24:22 +0100

    test changed, test.svg added to the project

--- tests/test_svg_handlers.py
+++ tests/test_svg_handlers.py
@@ -6,16 +6,13 @@ import os
 class TestSvgImport(TestCase):
 
     def test_owl_import_to_geometry(self):
-        ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-        path = os.path.join(ROOT_DIR, "examples/owl", "owl-svgrepo-com.svg")
-        geo = import_svg(path)
+        # path = os.path.join(ROOT_DIR, "examples/owl", "owl-svgrepo-com.svg")
+        geo = import_svg("test.svg")
 
         # checks the first coordinate of the first node
-        self.assertEqual(445.642, geo.nodes[0].x)
+        self.assertEqual(82.020832, geo.nodes[0].x)
 
         # the number of lines and cubicbeziers should be larger than 0
         self.assertTrue(len(geo.lines) > 0)
-        self.assertTrue(len(geo.cubic_beziers) > 0)
-
+        self.assertTrue(len(geo.cubic_beziers) == 0)
         self.assertTrue(len(geo.circle_arcs) == 0)
