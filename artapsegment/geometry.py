5144b2bcf34e9fafba60c89ad68c5737d7cbe97c
Tamas Orosz <oroszt@montana.hu>
2021-02-03 01:00:53 +0100

    can export bezier curves, owl shape

--- artapsegment/geometry.py
+++ artapsegment/geometry.py
@@ -128,21 +128,3 @@ def gmsh_strategy(nodes, lines, arcs, cubic_beziers):
         geom.save_geometry("test.geo_unrolled")
         #mesh = geom.generate_mesh()
         #mesh.write("test.vtk")
-
-# with pygmsh.geo.Geometry() as geom:
-#     lcar = 0.1
-#     p1 = geom.add_point([0.0, 0.0], lcar)
-#     p2 = geom.add_point([1.0, 0.0], lcar)
-#     p3 = geom.add_point([1.0, 0.5], lcar)
-#     p4 = geom.add_point([1.0, 1.0], lcar)
-#     s1 = geom.add_bspline([p1, p2, p3, p4])
-#
-#     p2 = geom.add_point([0.0, 1.0], lcar)
-#     p3 = geom.add_point([0.5, 1.0], lcar)
-#     s2 = geom.add_spline([p4, p3, p2, p1])
-#
-#     ll = geom.add_curve_loop([s1, s2])
-#     pl = geom.add_plane_surface(ll)
-#
-#     mesh = geom.generate_mesh()
-#     mesh.write("test.vtk")
