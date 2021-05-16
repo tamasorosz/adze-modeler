d48a5c18e46760ad35a199c09bd28b350d1e2b59
Tamas Orosz <oroszt@montana.hu>
2021-05-16 10:39:53 +0200

    test for femm execution

--- tests/test_femm_execution/test_femm_execution.py
+++ tests/test_femm_execution/test_femm_execution.py
@@ -1,5 +1,5 @@
 import unittest
-
+import os
 from adze_modeler.femm_wrapper import FemmExecutor
 
 
@@ -10,3 +10,18 @@ class TestFemmExecutor(unittest.TestCase):
         Runs a simple, built-in femm example: https://www.femm.info/wiki/CoilGun
         """
         FemmExecutor().run_femm('coilgun.lua')
+
+        with open('output.txt', 'r') as results:
+            content = results.readlines()
+
+            result = content[0].split(',')
+
+        self.assertEqual(1.5, float(result[0]))
+        self.assertEqual(0.05, round(float(result[1]), 2))
+
+        try:
+            os.remove('output.txt')
+            os.remove('temp.fem')
+            os.remove('temp.ans')
+        except FileExistsError:
+            print("The FEMM output files hadn't generated.")
